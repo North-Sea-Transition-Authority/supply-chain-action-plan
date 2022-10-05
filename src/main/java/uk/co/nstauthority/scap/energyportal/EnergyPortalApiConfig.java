6eef3888b2462b65af0078c3b5ebea75e68303a5
Alex McRae-Adams <alex.mcrae-adams@fivium.co.uk>
2022-10-05 15:05:21 +0100

    Declared EnergyPortal as Config

--- src/main/java/uk/co/nstauthority/scap/energyportal/EnergyPortalApiConfig.java
+++ src/main/java/uk/co/nstauthority/scap/energyportal/EnergyPortalApiConfig.java
@@ -2,8 +2,10 @@ package uk.co.nstauthority.scap.energyportal;
 
 import javax.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
 
+@Configuration
 @ConfigurationProperties(prefix = "energy-portal-api")
 @Validated
 public record EnergyPortalApiConfig(
