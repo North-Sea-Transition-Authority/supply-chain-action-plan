5b77e67c4b11a9509223f0bedf18ebcbe3b0e436
Alex McRae-Adams <alex.mcrae-adams@fivium.co.uk>
2022-10-05 16:17:03 +0100

    Checkstyle Issue

--- src/main/java/uk/co/nstauthority/scap/energyportal/EnergyPortalApiConfig.java
+++ src/main/java/uk/co/nstauthority/scap/energyportal/EnergyPortalApiConfig.java
@@ -4,6 +4,7 @@ import javax.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
+
 @ConfigurationProperties(prefix = "energy-portal-api")
 @Validated
 public record EnergyPortalApiConfig(
