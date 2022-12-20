5b77f49c808dfeff9b27a7b733562957afc2528b
Alex McRae-Adams <alex.mcrae-adams@fivium.co.uk>
2022-12-20 14:00:41 +0000

    Added SQL script for Regulator Team creation

---
+++ src/main/resources/db/migration/V3.0__CreateRegulatorTeam.sql
@@ -0,0 +1,3 @@
+/* Generate NSTA Regulator Team for Team Management */
+DELETE FROM scap.teams WHERE type = 'REGULATOR';
+INSERT INTO scap.teams (uuid, type, display_name, organisation_group_id) VALUES ('00000000-0000-0000-0000-000000000000', 'REGULATOR', 'NSTA', 1);
